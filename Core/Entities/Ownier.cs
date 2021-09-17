317c34b1b5ec0690cbaf6d4987652045308d0f14
Ahmed Khojaly <Ahmed Khojaly@DESKTOP-77EL5KR>
2021-09-17 21:48:27 +0200

    Add project files.

---
+++ Core/Entities/Ownier.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+    public class Ownier : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Job { get; set; }
+        public string Avatar { get; set; }
+        public string Address { get; set; }
+    }
+}
