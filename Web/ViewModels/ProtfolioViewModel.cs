77fd3e3e034a3bca339dc58c3adcc1de0d21273c
Ahmed Khojaly <Ahmed Khojaly@DESKTOP-A833B64>
2023-11-10 23:42:27 +0200

    Add ProtfolioViewModel

---
+++ Web/ViewModels/ProtfolioViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Web.ViewModels
+{
+    public class ProtfolioViewModel
+    {
+        public Guid Id { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ProjectImage { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
